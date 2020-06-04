df53076da3b7da7708dec50593c215266ef93908
nenistica <danielamolinaluna1993@gmail.com>
2020-06-04 08:59:20 -0300

    Se colocan @Columns en Reserva.

--- hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/entities/Reserva.java
+++ hoteltresvagos/src/main/java/ar/com/ada/hoteltresvagos/entities/Reserva.java
@@ -9,14 +9,24 @@ import javax.persistence.* ;
 @Table(name = "reserva")
 public class Reserva {
 
+    @Id
+    @Column(name = "reserva_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private int reservaId;
+    @Column(name = "fecha_reserva")
     private Date fechaReserva;
+    @Column(name = "fecha_ingreso")
     private Date fechaIngreso;
+    @Column(name = "fecha_egreso")
     private Date fechaEgreso;
-    private int habitacion;
+    private int habitacion; //No coloco anotación porque se llaman igual en la db
+    @Column(name = "importe_reserva")
     private double importeReserva;
+    @Column(name = "importe_total")
     private BigDecimal importeTotal;
+    @Column(name = "fecha_pagado")
     private BigDecimal importePagado;
+    @Column(name = "tipo_estado")
     private int tipoEstadoId; //Por ahora vamos a crear esto como int 
     private Huesped huesped;
 
